affe660c8167ce36397969fc9f853dde92309897
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-11-05 08:43:50 +0100

    add cis4 header switch to student view

--- views/cis/student.php
+++ views/cis/student.php
@@ -1,26 +1,38 @@
 <?php
-$this->load->view(
-	'templates/FHC-Header',
-	array(
-		'title' => 'International-Student',
-		'vue3' => true,
-		'bootstrap5' => true,
-		'primevue3' => true,
-		'tabulator5' => true,
-		'fontawesome6' => true,
-		'axios027' => true,
-		'phrases' => array(
-			'ui',
-			'international'
-		),
-		'customCSSs' => array(
-			'public/extensions/FHC-Core-International/css/student.css'
-		),
-		'customJSModules' => array(
-			'public/extensions/FHC-Core-International/js/apps/StudentApp.js',
-		),
-	)
+$includesArray = array(
+    'title' => 'International-Student',
+    'vue3' => true,
+    'bootstrap5' => true,
+    'primevue3' => true,
+    'tabulator5' => true,
+    'fontawesome6' => true,
+    'axios027' => true,
+    'phrases' => array(
+	    'ui',
+	    'international'
+    ),
+    'customCSSs' => array(
+	    'public/extensions/FHC-Core-International/css/student.css'
+    ),
+    'customJSModules' => array(
+	    'public/extensions/FHC-Core-International/js/apps/StudentApp.js',
+    ),
 );
+
+if(defined("CIS4"))
+{
+	$this->load->view(
+		'templates/CISVUE-Header',
+		$includesArray
+	);
+}
+else
+{
+	$this->load->view(
+		'templates/FHC-Header',
+		$includesArray
+	);
+}
 ?>
 <body>
 <div id="main">
@@ -30,6 +42,18 @@ $this->load->view(
 	></student>
 </div>
 
-<?php $this->load->view('templates/FHC-Footer'); ?>
+<?php
+if (defined("CIS4")) {
+	$this->load->view(
+		'templates/CISVUE-Footer',
+		$includesArray
+	);
+} else {
+	$this->load->view(
+		'templates/FHC-Footer', 
+		$includesArray
+	);
+}
+?>
 
 

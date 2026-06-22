a09f6e8c40d3f3313b1a820e8f324cc2038dfb40
Harald Bamberger <harald.bamberger@technikum-wien.at>
2026-06-22 15:11:52 +0200

    add cis4 header and footer

--- views/studiengangsleitung/studiengangsleitung.php
+++ views/studiengangsleitung/studiengangsleitung.php
@@ -1,26 +1,38 @@
 <?php
-$this->load->view(
-	'templates/FHC-Header',
-	array(
-		'title' => 'International-Studiengangsleitung',
-		'vue3' => true,
-		'bootstrap5' => true,
-		'primevue3' => true,
-		'tabulator5' => true,
-		'fontawesome6' => true,
-		'axios027' => true,
-		'filtercomponent' => true,
-		'phrases' => array(
-			'international',
-		),
-		'customCSSs' => array(
-			'public/extensions/FHC-Core-International/css/studiengangsleitung.css'
-		),
-		'customJSModules' => array(
-			'public/extensions/FHC-Core-International/js/apps/StudiengangsleitungApp.js',
-		)
+$includesArray = array(
+	'title' => 'International-Studiengangsleitung',
+	'vue3' => true,
+	'bootstrap5' => true,
+	'primevue3' => true,
+	'tabulator5' => true,
+	'fontawesome6' => true,
+	'axios027' => true,
+	'filtercomponent' => true,
+	'phrases' => array(
+		'international',
+	),
+	'customCSSs' => array(
+		'public/extensions/FHC-Core-International/css/studiengangsleitung.css'
+	),
+	'customJSModules' => array(
+		'public/extensions/FHC-Core-International/js/apps/StudiengangsleitungApp.js',
 	)
 );
+
+if(defined("CIS4") && CIS4)
+{
+	$this->load->view(
+		'templates/CISVUE-Header',
+		$includesArray
+	);
+}
+else
+{
+	$this->load->view(
+		'templates/FHC-Header',
+		$includesArray
+	);
+}
 ?>
 <body>
 <div id="main">
@@ -34,5 +46,17 @@ $this->load->view(
 </div>
 </body>
 
-<?php $this->load->view('templates/FHC-Footer'); ?>
+<?php
+if (defined("CIS4") && CIS4) {
+	$this->load->view(
+		'templates/CISVUE-Footer',
+		$includesArray
+	);
+} else {
+	$this->load->view(
+		'templates/FHC-Footer', 
+		$includesArray
+	);
+}
+?>
 
